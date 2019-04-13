accc57b814e76620d9e3e43ec243fbb67aafe0c9
Lee Williams <lelandhwilliams@gmail.com>
2019-04-12 20:35:05 -0500

    Seperated testfacts to their own file.

--- test.bat
+++ test.bat
@@ -1,20 +1,7 @@
 (load cards.clp)
 (load templates.clp)
+(export ?ALL)
 (load gameloop.clp)
+(load discard.clp)
+(load testfacts.clp)
 
-(deffacts testfacts
-	(player
-		(name "PlayerOne")
-		(playertype RandomPlayer)
-		(drawpile 1 2 3 4 5 6 7 8 9 10)
-	)
-	(player
-		(name "PlayerTwo")
-		(playertype RandomPlayer)
-		(drawpile 11 12 13 14 15 16 17 18 19 20)
-	)
-	(deck
-		(explorers 21 22 23 24 25 26 27 28 29 30)
-	)
-)
-		
---
+++ testfacts.clp
@@ -0,0 +1,16 @@
+(deffacts testfacts
+	(player
+		(name "PlayerOne")
+		(playertype RandomPlayer)
+		(drawpile 1 2 3 4 5 6 7 8 9 10)
+	)
+	(player
+		(name "PlayerTwo")
+		(playertype RandomPlayer)
+		(drawpile 11 12 13 14 15 16 17 18 19 20)
+	)
+	(deck
+		(explorers 21 22 23 24 25 26 27 28 29 30)
+	)
+)
+	
